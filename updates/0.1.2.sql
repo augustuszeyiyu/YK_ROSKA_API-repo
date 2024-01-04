ee02b3c3194b58e8b669d8bae6520bd3f66289e7
Cheny <linux.experts111@gmail.com>
2024-01-04 19:18:42 +0800

    update sql

--- updates/0.1.2.sql
+++ updates/0.1.2.sql
@@ -4,3 +4,14 @@ SET search_path TO "public";
 ALTER TABLE roska_groups  RENAME COLUMN IF EXISTS installment_amount TO win_amount;
 
 ALTER TABLE roska_members ADD COLUMN IF NOT EXISTS details      JSONB    NOT NULL DEFAULT '{}'::jsonb;
+
+
+CREATE OR REPLACE FUNCTION set_roska_groups_bid_end_time()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Set the time part to 23:59:59
+    NEW.bid_end_time := DATE_TRUNC('day', NEW.bid_start_time) + INTERVAL '4 days - 1 second';
+    NEW.installment_deadline := DATE_TRUNC('day', NEW.bid_end_time) + INTERVAL '6 days - 1 second';
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
